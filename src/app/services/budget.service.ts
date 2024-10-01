4dec6629b619ddb5604fd420e6bebf6f2f113363
Miranda Callejón <mirandacallejonh@gmail.com>
2024-10-01 13:53:21 +0200

    service budget added

---
+++ src/app/services/budget.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BudgetService {
+numPages: number = 0;
+numLanguages: number = 0;
+
+  constructor() { }
+}
