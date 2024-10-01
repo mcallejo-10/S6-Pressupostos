d0f3397469b16f49713d410c23a4b7528347855a
Miranda Callejón <mirandacallejonh@gmail.com>
2024-10-01 13:03:20 +0200

    Calcuate total ok!!!!

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  total: number = 0;
+  isCheckedSeo: boolean = false;
+  isCheckedAds: boolean = false;
+  isCheckedWeb: boolean = false;
+  
+  calculateTotal() {
+    this.total = 0;
+    if (this.isCheckedAds === true) {
+      this.total = this.total + 400;      
+    }
+    if (this.isCheckedSeo === true) {
+      this.total = this.total + 300;      
+    }
+    if (this.isCheckedWeb === true) {
+      this.total = this.total + 500;      
+    }
+  }
+  
 }
